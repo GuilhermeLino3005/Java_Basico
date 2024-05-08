05c16c6b2533d8dcf6b9f9a75049b78d2350584b
GuilhermeLino <joseguilhermelino3005@gmail.com>
2024-05-08 16:42:45 -0300

    Tratamento de exceções

---
+++ java/src/edu/guilherme/modulos/estruturasexcepcionais/CepInvalidoException.java
@@ -0,0 +1,7 @@
+package edu.guilherme.modulos.estruturasexcepcionais;
+
+public class CepInvalidoException extends Exception {
+     public static void main(String[] args) {
+          
+     }
+}
---
+++ java/src/edu/guilherme/modulos/estruturasexcepcionais/ExemploExececao.java
@@ -0,0 +1,16 @@
+package edu.guilherme.modulos.estruturasexcepcionais;
+
+import java.text.NumberFormat;
+import java.text.ParseException;;
+
+public class ExemploExececao {
+     public static void main(String[] args) {
+          /**try{
+               Number valor = NumberFormat.getInstance().parse("a1.75");
+               System.out.println(valor);
+          }
+          catch(ParseException e){
+               e.printStackTrace();
+          }*/
+     }
+}
