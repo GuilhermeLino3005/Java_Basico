77b4449acc2411cd2cfc0b9de86f18ca8987be11
GuilhermeLino <joseguilhermelino3005@gmail.com>
2024-06-05 06:59:40 -0300

    Aulas de Stream API

---
+++ java/src/edu/guilherme/modulos/streamapi/BinaryOperatorInterface.java
@@ -0,0 +1,27 @@
+package edu.guilherme.modulos.streamapi;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.BinaryOperator;
+
+/**
+ *   Representa uma operação que combina dois  argumentos do tipo T e retorna um resultado do mesmo tipo T.
+ *  É usada para realizar operações de redução em pares de elementos, como somar números ou combinar objetos.
+ */
+
+public class BinaryOperatorInterface {
+     public static void main(String[] args) {
+          //Criar uma lista de números inteiros
+          List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 5);
+
+          //Usar O BinaryOperator para somar todos os números no Stream
+          BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+
+          //Usar o BinaryOperator para somar todos os números no Stream
+          int resultado = numeros.stream()
+               .reduce(0, somar);
+          
+          //Imprimir o resultado da soma
+          System.out.println("A soma dos números é: " +resultado);
+     }
+}
